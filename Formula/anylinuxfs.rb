dff90993cd38cf00df4fb4d15ccbe3d09bd84341
Jan Noha <nohajc@gmail.com>
2025-04-20 18:20:48 +0200

    looks like everything is installed

--- Formula/anylinuxfs.rb
+++ Formula/anylinuxfs.rb
@@ -11,26 +11,41 @@ class Anylinuxfs < Formula
   # end
 
   depends_on "go" => :build
+  depends_on "make" => :build
   depends_on "messense/macos-cross-toolchains/aarch64-unknown-linux-musl" => :build
   depends_on "pkgconf" => :build
   depends_on "rustup" => :build
-  # Upstream only supports Hypervisor.framework on arm64
+
+  # libkrun only supports Hypervisor.framework on arm64
   depends_on arch: :arm64
+  depends_on :macos
   depends_on "slp/krun/libkrun"
   depends_on "util-linux"
 
-  # Additional dependency
-  # resource "" do
-  #   url ""
-  #   sha256 ""
-  # end
+  resource "gvproxy" do
+    url "https://github.com/containers/gvisor-tap-vsock/archive/refs/tags/v0.8.5.tar.gz"
+    sha256 "549dcb319fdd9813eba525bd67ad224a4b561211a940cbdc0e2c94bd2b59b9c9"
+  end
+
+  resource "linux-image" do
+    url "https://github.com/nohajc/libkrunfw/releases/download/v6.6-nfs/linux-aarch64-Image-v6.6-nfs.tar.gz"
+    sha256 "fb660a9d1d9ab6c6a0b2e4b48a7777b5ddddc739a244a6f4e4551c8d62a93808"
+  end
 
   def install
     system "rustup", "default", "stable"
-    # system "rustup", "target", "add", "aarch64-apple-darwin"
     system "rustup", "target", "add", "aarch64-unknown-linux-musl"
     system "./build-app.sh", "--release"
     system "./install.sh", prefix
+
+    resource("gvproxy").stage do
+      system "gmake", "gvproxy"
+      libexec.install "bin/gvproxy"
+    end
+
+    resource("linux-image").stage do
+      libexec.install "Image"
+    end
   end
 
   test do

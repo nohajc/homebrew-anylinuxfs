31c6aea074cde9862514ebf59498a9fbef60262a
Jan Noha <nohajc@gmail.com>
2025-05-07 21:51:33 +0200

    install-and-build-bottle.sh: update bottle hash automatically

--- install-and-build-bottle.sh
+++ install-and-build-bottle.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 brew install --build-bottle anylinuxfs
-brew bottle anylinuxfs
+NEW_BOTTLE_HASH=$(brew bottle anylinuxfs | grep 'sha256 cellar:' | grep -oE '[a-f0-9]{64}')
+perl -pi -e 's/sha256 cellar: :any, arm64_sequoia: "[a-f0-9]{64}"/sha256 cellar: :any, arm64_sequoia: "'$NEW_BOTTLE_HASH'"/' Formula/anylinuxfs.rb
 for f in anylinuxfs--*; do mv "$f" $(echo $f | sed 's/--/-/'); done

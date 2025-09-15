330a432fee132df090a7ae6d505c8d0c3b39eaba
Jan Noha <nohajc@gmail.com>
2025-09-15 14:52:53 +0200

    install-and-build-bottle.sh: make it work for different macOS versions

--- install-and-build-bottle.sh
+++ install-and-build-bottle.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+macos_codename() {
+  case "$(sw_vers -productVersion | cut -d. -f1)" in
+    13) echo "ventura" ;;
+    14) echo "sonoma" ;;
+    15) echo "sequoia" ;;
+    16) echo "tahoe" ;;
+    *)  echo "unknown" ;;
+  esac
+}
+
+OS_NAME=$(macos_codename)
+
 brew install --build-bottle anylinuxfs
 NEW_BOTTLE_HASH=$(brew bottle anylinuxfs | grep 'sha256 cellar:' | grep -oE '[a-f0-9]{64}')
-perl -pi -e 's/sha256 cellar: :any, arm64_sequoia: "[a-f0-9]{64}"/sha256 cellar: :any, arm64_sequoia: "'$NEW_BOTTLE_HASH'"/' Formula/anylinuxfs.rb
+perl -pi -e 's/sha256 cellar: :any, arm64_$OS_NAME: "[a-f0-9]{64}"/sha256 cellar: :any, arm64_$OS_NAME: "'$NEW_BOTTLE_HASH'"/' Formula/anylinuxfs.rb
 for f in anylinuxfs--*; do mv "$f" $(echo $f | sed 's/--/-/'); done

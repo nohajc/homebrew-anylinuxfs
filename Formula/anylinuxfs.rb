a97f878a08feceecc0bc2be7ca5758f71dc5c321
Jan Noha <nohajc@gmail.com>
2026-01-09 23:40:38 +0100

    v0.9.3 tahoe

--- Formula/anylinuxfs.rb
+++ Formula/anylinuxfs.rb
@@ -1,15 +1,15 @@
 class Anylinuxfs < Formula
-  VERSION = "0.9.2".freeze
+  VERSION = "0.9.3".freeze
 
   desc "Mount any linux-supported filesystem read/write using nfs and a microVM"
   homepage "https://github.com/nohajc/anylinuxfs"
   url "https://github.com/nohajc/anylinuxfs/archive/refs/tags/v#{VERSION}.tar.gz"
-  sha256 "148105cc9c35b2737ddc1e2d4cf17595ce2687a443ae543d5b20934b0da5302d"
+  sha256 "fdf5250712ae5c7bf712ae857054c41898ae294b4ac08ad258ae8061080d59b7"
   license "GPL-3.0-or-later"
 
   bottle do
     root_url "https://github.com/nohajc/homebrew-anylinuxfs/releases/download/v#{VERSION}"
-    sha256 cellar: :any, arm64_tahoe:   "feea4ea7ba593979d3c34929f907097ed7c889ebc1647873dd48183d9abac6ff"
+    sha256 cellar: :any, arm64_tahoe:   "b08b1b3ef3d9c6ea2396ff5af661fb8cd4675d7729cadbb184c9a02993fd9ea4"
     sha256 cellar: :any, arm64_sequoia: "eb5bba17943e4c60d26a3fcfa56ed0b7f0d93d5d71f66a8276a1ea0495c7a28a"
   end
 
